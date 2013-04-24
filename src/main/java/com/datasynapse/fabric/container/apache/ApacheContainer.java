74c569cb36f176780583f3ba78aeec84246e575f
Ethan Goodman <egoodman@tibco.com>
2013-04-24 15:24:26 -0400

    added copyright header text to .java files

--- src/main/java/com/datasynapse/fabric/container/apache/ApacheContainer.java
+++ src/main/java/com/datasynapse/fabric/container/apache/ApacheContainer.java
@@ -1,3 +1,10 @@
+/*
+ * Copyright (c) 2013 TIBCO Software Inc. All Rights Reserved.
+ * 
+ * Use is subject to the terms of the TIBCO license terms accompanying the download of this code. 
+ * In most instances, the license terms are contained in a file named license.txt.
+ */
+
 package com.datasynapse.fabric.container.apache;
 
 import java.io.File;
